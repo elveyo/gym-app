722bd744d227e52db38acfc74a978ab8130f9f1c
elveyo <elviragic33@gmail.com>
2022-08-21 11:32:29 +0200

    Home page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>hell0</h1>);
+root.render(<App />);
